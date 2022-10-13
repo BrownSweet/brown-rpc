a0dc3f40b61ad70f9e54b6b1901d423cb6bbdb30
brown_sweet <455764041@qq.com>
2022-10-13 23:02:42 +0800

    新增了协程处理队列内容

--- src/server/queue/Queue.php
+++ src/server/queue/Queue.php
@@ -5,6 +5,7 @@ use app\controller\Client;
 use app\controller\Cmq;
 use brown\cmq\Account;
 use brown\cmq\CMQExceptionBase;
+use Swoole\Coroutine;
 use Swoole\Process;
 use TencentCloud\Common\Credential;
 use TencentCloud\Common\Exception\TencentCloudSDKException;
@@ -39,35 +40,41 @@ trait Queue{
                     $queueHandle=$worker->reciveMessage($listen);
 
                     if (!$queueHandle instanceof CMQExceptionBase){
-                        $this->logger->info($queueHandle.PHP_EOL);
-                        $msgBody=json_decode($queueHandle->msgBody,true);
-                        $autoAck=$msgBody['AutoAck'];
-                        if (isset($msgBody['notifyUrl'])){
-                            $url=$msgBody['notifyUrl'];
-                            unset($msgBody['notifyUrl']);
-                            unset($msgBody['AutoAck']);
-                            $code=SendRequest($url,json_encode($msgBody),[
-                                'user-agent'   => 'Send Notification Brwon Queue Service Agent',
-                                'content-type' => 'text/plain; charset=utf-8',
-                                'x-cmq-message-id'=>$queueHandle->msgId,
-                                'x-cmq-receipt-handle'=>$queueHandle->receiptHandle,
-                                'x-cmq-enqueue-time'=>$queueHandle->enqueueTime,
-                                'x-cmq-queue-name'=>$listen,
-                                'x-cmq-queue-ack'=>$autoAck
-                            ]);
-                        }
+                        $cid=Coroutine::create(function () use ($listen,$queueHandle,$worker) {
+                            $msgBody=json_decode($queueHandle->msgBody,true);
+                            $autoAck=$msgBody['AutoAck'];
+                            if (isset($msgBody['notifyUrl'])){
+                                $url=$msgBody['notifyUrl'];
+                                unset($msgBody['notifyUrl']);
+                                unset($msgBody['AutoAck']);
+                                $data=json_encode($msgBody);
+                                $header=[
+                                    'user-agent'   => 'Send Notification Brwon Queue Service Agent',
+                                    'content-type' => 'text/plain; charset=utf-8',
+                                    'x-cmq-message-id'=>$queueHandle->msgId,
+                                    'x-cmq-receipt-handle'=>$queueHandle->receiptHandle,
+                                    'x-cmq-enqueue-time'=>$queueHandle->enqueueTime,
+                                    'x-cmq-queue-name'=>$listen,
+                                    'x-cmq-queue-ack'=>$autoAck
+                                ];
+                                $this->logger->info('开启处理协程,协程的父Id：'.Coroutine::getPcid().' 协程的Id'.Coroutine::getCid());
+                                $this->logger->info($queueHandle.PHP_EOL);
+                                $code=SendRequest($url,$data,$header);
+                                if ($autoAck==QueueConstant::AUTO_ACK){
+                                    $worker->AcknowledgeMessage($listen,$queueHandle);
+                                }
+                                if ($autoAck==QueueConstant::STATUS_CODE_ACK){
+                                    if ($code>=200&&$code<400){
+                                        $worker->AcknowledgeMessage($listen,$queueHandle);
+                                    }
+                                }
+                                if ($autoAck==QueueConstant::MANNUL_ACK){
 
-                        if ($autoAck==QueueConstant::AUTO_ACK){
-                            $worker->AcknowledgeMessage($listen,$queueHandle);
-                        }
-                        if ($autoAck==QueueConstant::STATUS_CODE_ACK){
-                            if ($code>=200&&$code<400){
-                                $worker->AcknowledgeMessage($listen,$queueHandle);
+                                }
                             }
-                        }
-                        if ($autoAck==QueueConstant::MANNUL_ACK){
+                        });
+                        $this->logger->info('内存使用量：'.Coroutine::getStackUsage());
 
-                        }
                     }else{
 
                     }
@@ -76,5 +83,5 @@ trait Queue{
 
         }
 
-    }
+
 }
