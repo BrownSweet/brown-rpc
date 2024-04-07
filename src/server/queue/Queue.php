e87439e556f0e292f1956eed30bc40c7c4eecc63
brown_sweet <455764041@qq.com>
2024-04-07 12:08:49 +0800

    1、修改了队列的实现逻辑,增加了异常处理提醒

--- src/server/queue/Queue.php
+++ src/server/queue/Queue.php
@@ -37,15 +37,23 @@ trait Queue{
         $handlers=$this->getConfig('queue.handlers');
         $ack=$this->getConfig('queue.ack');
 
-        foreach ($workers as $worker) {
-            foreach ($listens as $listen) {
-                $this->addMoreWorker($workerNum, function (Process\Pool $pool) use ($workers, $listen,$handlers,$ack) {
+
+        foreach ($listens as $listen) {
+            $this->addMoreWorker($workerNum, function (Process\Pool $pool) use ($workers, $listen,$handlers,$ack) {
                     $this->logger->info('监听队列：' . $listen . PHP_EOL);
                     while (true) {
+                        if (!isset($workers[$listen])){
+                            $this->logger->info('队列监听器['.$listen.']不存在，退出['.$listen.']监听' . PHP_EOL);
+                            continue;
+                        }
                         $queueHandle = $workers[$listen]->reciveMessage($listen);
                         foreach ($handlers[$listen] as $handler){
-
+                            if (!class_exists($handler)) {
+                                $this->logger->error("处理器类 {$handler} 不存在");
+                                continue;
+                            }
                             $handler_obj=new $handler();
+
                             if ($handler_obj instanceof QueueHandler){
                                 $cid = Coroutine::create(function () use ($listen, $queueHandle, $workers,$handler_obj) {
                                     $handler_obj->handle($listen, $queueHandle);
@@ -57,6 +65,10 @@ trait Queue{
 
                         if (array_key_exists($listen,$ack)){
                             foreach ($ack[$listen] as $ackItem){
+                                if (!class_exists($ackItem)) {
+                                    $this->logger->error("确认类 {$ackItem} 不存在");
+                                    continue;
+                                }
                                 $ackItem_obj=new $ackItem();
                                 if ($ackItem_obj instanceof QueueAcknowledge){
                                     $cid = Coroutine::create(function () use ($listen, $queueHandle, $workers,$ackItem_obj) {
@@ -69,9 +81,9 @@ trait Queue{
                         }
                     }
                 },'queue_server');
-            }
-
         }
+
+
 
 
     }
