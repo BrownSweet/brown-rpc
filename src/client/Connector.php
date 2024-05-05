afcbb9315f3f698e9af5d3628f39978c16fb11e6
brown_sweet <455764041@qq.com>
2024-05-05 22:26:19 +0800

    修改了，新增http2 客户端

--- src/client/Connector.php
+++ src/client/Connector.php
@@ -14,6 +14,8 @@ use brown\sendfile\FileBase;
 use GuzzleHttp\Exception\ConnectException;
 use Swoole\Client;
 use brown\exceptions\RpcException;
+use const http\Client\Curl\HTTP_VERSION_2_0;
+
 trait Connector
 {
     protected $parser;
@@ -37,15 +39,16 @@ trait Connector
         }
 
         $timeout=$this->getConfig('rpc.client.timeout');
-        if ($proto=='tcp'){
-            $client=new Client(SWOOLE_SOCK_TCP);
-            if (!$client->connect($host,$port,$timeout)){
-                throw new RpcException("连接失败");
-            }
-        }else{
-            $client = new \GuzzleHttp\Client(['base_uri'=>$host.':'.$port,'timeout'=>$timeout]);
 
-        }
+        $client = new \GuzzleHttp\Client(['base_uri'=>$host.':'.$port,'timeout'=>$timeout,
+            'curl' => [ // cURL选项
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_PRIOR_KNOWLEDGE, // 告诉cURL优先使用HTTP/2，但注意这可能会导致与不支持HTTP/2的服务器通信失败
+            ],]);
+//        $client=new Client(SWOOLE_SOCK_TCP);
+//        if (!$client->connect($host,$port,$timeout)){
+//            throw new RpcException("连接失败");
+//        }
+
         return $client;
     }
 
@@ -59,29 +62,62 @@ trait Connector
 
         $data=$this->encodeData($request,$this->parser);
         $conn=$this->connect($request->getProtocol());
+        $request = new \GuzzleHttp\Psr7\Request('POST', '/', [], $data);
+//
+        $response = $conn->send($request);
+//
+        $result = unserialize($response->getBody()->getContents());
 
-        if ($conn instanceof \GuzzleHttp\Client){
-            $request = new \GuzzleHttp\Psr7\Request('POST', '/', [], $data);
-
-            $response = $conn->send($request);
-
-            $result = unserialize($response->getBody()->getContents());
-        }elseif ($conn instanceof Client){
-            if (!$data instanceof \Generator){
-                $data=[$data];
-            }
-
-            foreach ($data as $string) {
-
-                if (!empty($string)) {
-
-                    if ($conn->send($string) === false) {
-                        throw new RpcException('Send data failed. ' .  $conn->errCode);
-                    }
-                }
-            }
-            $result=unserialize($conn->recv(65536,Client::MSG_WAITALL));
+        die;
+        if ($request->getProtocol()=='http'){
+            // 构建HTTP POST请求
+            $post_data = $data;
+            $http_request = "POST / HTTP/1.1\r\n";
+            $http_request .= "Host: www.example.com\r\n";
+            $http_request .= "Content-Type: application/x-www-form-urlencoded\r\n";
+            $http_request .= "Content-Length: " . strlen($post_data) . "\r\n";
+            $http_request .= "Connection: close\r\n";
+            $http_request .= "User-Agent: brown-rpc-client\r\n";
+            $http_request .= "\r\n";
+            $http_request .= $post_data;
+            $data=$http_request;
         }
+
+
+//        if ($conn instanceof \GuzzleHttp\Client){
+//            $request = new \GuzzleHttp\Psr7\Request('POST', '/', [], $data);
+//
+//            $response = $conn->send($request);
+//
+//            $result = unserialize($response->getBody()->getContents());
+//        }elseif ($conn instanceof Client){
+//            if (!$data instanceof \Generator){
+//                $data=[$data];
+//            }
+//
+//            foreach ($data as $string) {
+//
+//                if (!empty($string)) {
+//
+//                    if ($conn->send($string) === false) {
+//                        throw new RpcException('Send data failed. ' .  $conn->errCode);
+//                    }
+//                }
+//            }
+//            print_r($data);
+//            die;
+        if ($conn->send($data) === false) {
+            throw new RpcException('Send data failed. ' .  $conn->errCode);
+        }
+        $res=$conn->recv(65536,Client::MSG_WAITALL);
+        if ($request->getProtocol()=='http'){
+            $parts = explode("\r\n\r\n", $res, 2);
+            $res = $parts[1];
+        }
+
+        $result=unserialize($res);
+//            $result=unserialize($conn->recv(65536,Client::MSG_WAITALL));
+//        }
 
 //        try {
 //            $conn=$this->connect('http');
